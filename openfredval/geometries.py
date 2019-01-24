3f97ec495437f08e2e17d1e54458f65a80c7b679
Inia Steinbach <RL-INSTITUT\inia.steinbach@pc-44lin.rl-institut.local>
2019-01-24 16:32:26 +0100

    'deflex'changed to 'openfredval' and added paths to ini-file

--- openfredval/geometries.py
+++ openfredval/geometries.py
@@ -19,26 +19,30 @@ from reegis import config as cfg
 from reegis import geometries as geo
 
 
-def deflex_regions(suffix='reegis', rmap=None, rtype='polygon'):
+def openfredval_regions(suffix='reegis', rmap=None, rtype='polygon'):
     if rmap is None:
         rmap = cfg.get('init', 'map')
-    name = os.path.join(cfg.get('paths', 'geo_deflex'),
-                        cfg.get('geometry', 'deflex_polygon').format(
+    name = os.path.join(cfg.get('paths', 'geo_openfredval'),
+                        cfg.get('geometry', 'openfredval_polygon').format(
                             suffix=suffix, map=rmap, type=rtype))
-    regions = geo.load(fullname=name)
+    regions = geo.Geometry(name='{map}_region'.format(
+        map=rmap))
+    regions.load(fullname=name)
 
     # Add 'DE' and leading zero to index
-    regions['region'] = regions.index.to_series().astype(str).apply(
+    regions.gdf['region'] = regions.gdf.index.to_series().astype(str).apply(
         'DE{:0>2}'.format)
-    regions = regions.set_index('region')
+    regions.gdf = regions.gdf.set_index('region')
     return regions
 
 
-def deflex_power_lines(rmap=None, rtype='lines'):
+def openfredval_power_lines(rmap=None, rtype='lines'):
     if rmap is None:
         rmap = cfg.get('init', 'map')
-    name = os.path.join(cfg.get('paths', 'geo_deflex'),
+    name = os.path.join(cfg.get('paths', 'geo_openfredval'),
                         cfg.get('geometry', 'powerlines').format(
                             map=rmap, type=rtype))
-    lines = geo.load(fullname=name)
+    lines = geo.Geometry(name='{map}_region'.format(
+        map=rmap))
+    lines.load(fullname=name)
     return lines
