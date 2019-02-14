479db168039f3a607760310479976709f084dff1
SabineH <sabine.haas@rl-institut.de>
2019-02-14 11:53:43 +0100

    Start script for opsd power plants

---
+++ feedin_germany/opsd_power_plants.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+
+todo: module description
+
+The code in this module is partly based on third party code which has been
+licensed under GNU-LGPL3. The following functions are copied and adapted from:
+https://github.com/reegis/reegis
+* todo add functions - change text if just copied, not adapted
+"""
+
+# imports
+
+# feedin_germany imports
+
+
