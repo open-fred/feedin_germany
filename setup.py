47e78a87d6e91ac48e334bc223715f41788b4f87
SabineH <sabine.haas@rl-institut.de>
2019-02-14 10:06:16 +0100

    Add setup file

---
+++ setup.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='feedin_germany',
+    version='0.0.1',
+    author='oemof developing group',
+    author_email='',
+    description='Creating time series of renewable power plants for regions in Germany.',
+    packages=['feedin_germany'],
+    long_description=read('README.rst'),
+    install_requires=[
+        'pandas >= 0.13.1',
+        'feedinlib[Windpowerlib, PVlib]'])
