e2bb5c5df1ede95ebe9e4a6536e5fb1601debb04
SabineH <sabine.haas@rl-institut.de>
2019-02-25 11:28:45 +0100

    Add error raising

--- feedin_germany/test_weather_locations_to_register_in_feedinlib.py
+++ feedin_germany/test_weather_locations_to_register_in_feedinlib.py
@@ -5,6 +5,8 @@ import pandas as pd
 
 # loading weather data
 filename = os.path.abspath('/home/sabine/rl-institut/04_Projekte/163_Open_FRED/03-Projektinhalte/AP2 Wetterdaten/open_FRED_TestWetterdaten_csv/fred_data_2016_sh.csv')
+if not os.path.isfile(filename):
+    raise FileNotFoundError("Please adjust the path.")
 weather_df = pd.read_csv(filename,
                          header=[0, 1], index_col=[0, 1, 2],
                          parse_dates=True)
