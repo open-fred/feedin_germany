65f71058e308aa2bf393b1ab8c9b4db3ed67a656
Inia Steinbach <RL-INSTITUT\inia.steinbach@pc-44lin.rl-institut.local>
2019-02-26 15:17:14 +0100

    function create_pvmodule_dict() corrected

--- feedin_germany/pv_modules.py
+++ feedin_germany/pv_modules.py
@@ -6,32 +6,28 @@ Created on Thu Feb 14 16:25:35 2019
 @author: RL-INSTITUT\inia.steinbach
 """
 # Internal modules
-import config
+from feedin_germany import config
 # Python libraries
 import os
 import logging
 import collections
 
 
+
 def create_pvmodule_dict():
     
     pvlib_list = config.get('solar_sets', 'set_list')
     pvlib_sets = config.aslist(pvlib_list, flatten=True)
-    pvsets = collections.OrderedDict()
+    modules = collections.OrderedDict()
     
     for pvlib_set in pvlib_sets:
-        set_name = config.get(pvlib_set, 'pv_set_name')
-        module_name = config.get(pvlib_set, 'module_name')
-        inverter_name = config.get(pvlib_set, 'inverter_name')
-        azimuth = config.get(pvlib_set, 'azimuth')
-        tilt = config.get(pvlib_set, 'tilt')
-        albedo = config.get(pvlib_set, 'albedo')
-        
-        pvsets[set_name] = collections.OrderedDict()
-        content = module_name, inverter_name, azimuth, tilt, albedo
-        pvsets[set_name] = {content}
-        pvsets = collections.OrderedDict(pvsets)
-    return pvsets
+        modules[pvlib_set]= config.as_dict(pvlib_set)
+        #content = module_name, inverter_name, azimuth, tilt, albedo
+        #pvsets[set_name] = {content}
+        #pvsets = collections.OrderedDict(pvsets)
+    return modules
+
+
 
 def create_distribution_dict():
     
@@ -47,6 +43,6 @@ def parse_module_dict():
 
 if __name__ == "__main__":
     
-    print(parse_module_dict())
+    print(create_pvmodule_dict2())
     #pvsets=create_pvmodule_dict()
     #print(pvsets['BP2150S_3'])
