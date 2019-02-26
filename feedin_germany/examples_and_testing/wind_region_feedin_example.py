2157d39c0d49fc9fe95dadb60d46c6660b24fc11
SabineH <sabine.haas@rl-institut.de>
2019-02-26 16:46:19 +0100

    Add example for testing and development

---
+++ feedin_germany/examples_and_testing/wind_region_feedin_example.py
@@ -0,0 +1,25 @@
+
+# imports
+import pandas as pd
+import os
+from feedinlib import tools
+from feedinlib import region
+
+# import internal modules
+from feedin_germany import opsd_power_plants as opsd
+# import opsd_power_plants as opsd
+
+# get example weather
+filename = os.path.abspath('/home/sabine/rl-institut/04_Projekte/163_Open_FRED/03-Projektinhalte/AP2 Wetterdaten/open_FRED_TestWetterdaten_csv/fred_data_2016_sh.csv')
+weather_df = tools.example_weather_wind(filename)
+
+# get opsd register
+keep_cols = ['lat', 'lon', 'commissioning_date', 'capacity',
+             'com_year', 'decom_year', 'com_month', 'decom_month']
+register = opsd.filter_pp_by_source_and_year(year=2012, energy_source='Wind',
+                                             keep_cols=keep_cols)
+
+# feedinlib region feed-in
+example_region = region.Region(geom='no_geom', weather=weather_df).wind_feedin(register)
+
+
