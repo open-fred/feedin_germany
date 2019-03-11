44b7929e300e727a7fccaf58483d8008b3213042
SabineH <sabine.haas@rl-institut.de>
2019-03-11 17:48:55 +0100

    Start module for validation data

---
+++ feedin_germany/validation_data.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+The `validation_data` module contains functions ....
+
+"""
+
+__copyright__ = "Copyright oemof developer group"
+__license__ = "GPLv3"
+
+# imports
+import pandas as pd
+import geopandas as gpd
+import os
+import logging
+
+
+
+def load_feedin_data(): # _from_...? todo get data
+    pass
+    # load übertragunsnetz feed-in time series
+    # prepare like our feed-in time series (columns: time, technology, nuts, feedin_val)
+    # Zwischenspeichern ? je nachdem wie schnell...
