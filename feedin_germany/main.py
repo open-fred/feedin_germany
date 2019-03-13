c2c692a3fb75b5a1908f2b0a45b94c6b1e14654d
Inia Steinbach <RL-INSTITUT\inia.steinbach@pc-44lin.rl-institut.local>
2019-03-13 16:33:53 +0100

    solar + plot

--- feedin_germany/main.py
+++ feedin_germany/main.py
@@ -9,16 +9,17 @@ debug_mode = True
 
 years = [2012]
 categories = [
-    'Wind',
-    # 'Solar',
+    #'Wind',
+    'Solar',
     # 'Hydro'
 ]
 for year in years:
         feedin = feedin.calculate_feedin_germany(
             year=year, categories=categories, regions='landkreise',
             register_name='opsd', weather_data_name='open_FRED',
-            oep_upload=True, debug_mode=debug_mode, wake_losses_model=None)
+            oep_upload=False, debug_mode=debug_mode, wake_losses_model=None)
         print(feedin)
 
-        # feedin.plot()
-        # plt.show()
+        feedin.plot()
+        plt.ylabel('AC Power (W)')
+        plt.show()
