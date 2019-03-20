f6643938d7f66c5fe9966c0c372486db8bb8292f
Inia Steinbach <RL-INSTITUT\inia.steinbach@pc-44lin.rl-institut.local>
2019-03-20 11:40:42 +0100

    change to pv_region

--- feedin_germany/feedin.py
+++ feedin_germany/feedin.py
@@ -120,8 +120,7 @@ def calculate_feedin(year, register, regions, category, return_feedin=False,
                     ['lat', 'lon', 'commissioning_date', 'capacity',
                      'Coordinates']]
                 # open feedinlib to calculate feed in time series for region
-                feedin = region.Region(geom='no_geom',
-                                       weather=weather_pv).pv_feedin_distribution_register(
+                feedin = pv_region.pv_feedin_distribution_register(
                     distribution_dict=distribution_dict,
                     technical_parameters=pv_modules_set, register=register_pv)
             elif category == 'Wind':
