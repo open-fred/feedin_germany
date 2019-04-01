94e9dfbb5a26a90072e047490a1ac90175a085a8
SabineH <sabine.haas@rl-institut.de>
2019-04-01 17:13:00 +0200

    Finish MaStR data

--- feedin_germany/mastr_power_plants.py
+++ feedin_germany/mastr_power_plants.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-todo add description
+The `mastr_power_plant` module contains functions for downloading and
+processing renewable power plant data for Germany from the
+Markstammdatenregister (MaStR). todo source orginial + OEP
 
 """
 
@@ -39,7 +41,6 @@ def load_mastr_data_from_oedb():
 
     todo: use sessionmaker um Anlagen auszuwählen
 
-    todo: Inhalt nicht wie erwartet.... Ludwig??
     Returns
     -------
 
@@ -66,6 +67,13 @@ def helper_load_mastr_from_file(category):
     r"""
     todo remove when loaded from oedb
 
+    andere interessante Spalten könnten sein (gerade keine Einträge):
+    - Erzeugungsleistung
+
+    Notes
+    -----
+    - Manche "Bruttoleistungen" scheinen falsch zu sein: 0 oder krumme Zahl.
+
     Parameters
     ----------
     category : string
@@ -76,22 +84,29 @@ def helper_load_mastr_from_file(category):
     -------
 
     """
-    filename = os.path.join(
-        '~/Daten_flexibel_01/bnetza_mastr/bnetza_mastr_power-units_v1.2/',
-        'bnetza_mastr_1.2_wind.csv'.format(category.lower()))
-    usecols = [
-        'Nabenhoehe', 'Rotordurchmesser',
-        # 'HerstellerName', 'Einheitart', 'Einheittyp', 'Technologie',
-        'Typenbezeichnung', 'Laengengrad', 'Breitengrad',
-        'Inbetriebnahmedatum', 'DatumEndgueltigeStilllegung',
-        'DatumBeginnVoruebergehendeStilllegung', 'DatumWiederaufnahmeBetrieb'
-               ]
+    if category == 'Wind':
+        filename = os.path.join(
+            '~/Daten_flexibel_01/bnetza_mastr/bnetza_mastr_power-units_v1.2/',
+            'bnetza_mastr_1.2_wind.csv'.format(category.lower()))
+        usecols = [
+            'Nabenhoehe', 'Rotordurchmesser',
+            # 'HerstellerName', 'Einheitart', 'Einheittyp', 'Technologie',
+            'Typenbezeichnung', 'Laengengrad', 'Breitengrad',
+            'Inbetriebnahmedatum', 'DatumEndgueltigeStilllegung',
+            'DatumBeginnVoruebergehendeStilllegung',
+            'DatumWiederaufnahmeBetrieb', 'Bruttoleistung'
+                ]
+    elif category == 'Solar':
+        raise ValueError("Solar MaStR data not added, yet.")
+    else:
+        raise ValueError("Category {} not existent. ".format(category) +
+                         "Choose from: 'Wind', ...") # todo add
     mastr_data = pd.read_csv(filename, sep=';', encoding='utf-8',
                              header=0, usecols=usecols)
     return mastr_data
 
 
-def prepare_mastr_data(mastr_data):
+def prepare_mastr_data(mastr_data, category):
     r"""
 
     - translation to english
@@ -111,26 +126,48 @@ def prepare_mastr_data(mastr_data):
     mastr_data : pd.DataFrame
         Contains raw MaStR data as loaded in
         `:py:func:helper_load_mastr_from_file`.
+    category : string
+        Energy source category for which the register is loaded. Options:
+        'Wind', ... to be added.
 
     Returns
     -------
 
     """
-    mastr_data.rename(columns={
-        'Nabenhoehe': 'hub_height', 'Rotordurchmesser': 'rotor_diameter',
-        # 'HerstellerName', 'Einheitart', 'Einheittyp', 'Technologie',
-        'Typenbezeichnung': 'turbine_type', 'Laengengrad': 'lon',
-        'Breitengrad': 'lat', 'Inbetriebnahmedatum': 'commissioning_date',
-        'DatumEndgueltigeStilllegung': 'decommissioning_date',
-        'DatumBeginnVoruebergehendeStilllegung': 'temporary_decom_date',
-        'DatumWiederaufnahmeBetrieb': 'resumption_date'
-    }, inplace=True)
+    if category == 'Wind':  # todo: add 'name' column of name matching from Ludwig!!!
+        mastr_data.rename(columns={
+            'Nabenhoehe': 'hub_height', 'Rotordurchmesser': 'rotor_diameter',
+            # 'HerstellerName', 'Einheitart', 'Einheittyp', 'Technologie',
+            'Typenbezeichnung': 'turbine_type', 'Laengengrad': 'lon',
+            'Breitengrad': 'lat', 'Inbetriebnahmedatum': 'commissioning_date',
+            'DatumEndgueltigeStilllegung': 'decommissioning_date',
+            'DatumBeginnVoruebergehendeStilllegung': 'temporary_decom_date',
+            'DatumWiederaufnahmeBetrieb': 'resumption_date',
+            'Bruttoleistung': 'capacity'
+        }, inplace=True)
     #
     date_cols = ('commissioning_date', 'decommissioning_date')
     prepared_df = ppr_tools.prepare_dates(df=mastr_data, date_cols=date_cols,
                                           month=True)
     return prepared_df
 
+
+def get_mastr_pp_filtered_by_year(category, year):
+    r"""
+
+
+    """
+    mastr_pp = helper_load_mastr_from_file(category=category)
+    prepared_data = prepare_mastr_data(mastr_pp, category)
+    filtered_register = ppr_tools.get_pp_by_year(year=year,
+                                                 register=prepared_data)
+    filtered_register = ppr_tools.remove_pp_with_missing_coordinates(
+        register=filtered_register, category=category, register_name='MaStR')
+    return filtered_register
+
+
 if __name__ == "__main__":
-    mastr_data = helper_load_mastr_from_file(category='Wind')
-    prepared_data = prepare_mastr_data(mastr_data)
+    year = 2012
+    cat = 'Wind'
+    mastr_pp = get_mastr_pp_filtered_by_year(category=cat, year=year)
+    print(mastr_pp.head())
