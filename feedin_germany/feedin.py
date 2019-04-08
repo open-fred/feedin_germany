b90b6e7a9a0c48c1e26456655739fdec7a0498f0
SabineH <sabine.haas@rl-institut.de>
2019-04-08 16:53:39 +0200

    Add technology column to keep_cols

--- feedin_germany/feedin.py
+++ feedin_germany/feedin.py
@@ -7,6 +7,8 @@ downloads data needed for the calculations.
 
 """
 
+#todo feedinlib: wetterdaten UTC? falls nicht aufpassen bei Validierung
+
 __copyright__ = "Copyright oemof developer group"
 __license__ = "GPLv3"
 
@@ -252,7 +254,8 @@ def calculate_feedin_germany(year, categories, regions='landkreise',
     for category in categories:
         # get power plant register for all power plants in Germany
         if register_name == 'opsd':
-            keep_cols = ['lat', 'lon', 'commissioning_date', 'capacity']
+            keep_cols = ['lat', 'lon', 'commissioning_date', 'capacity',
+                         'technology']  # technology needed for offshore wind
             register = opsd.filter_pp_by_source_and_year(year, category,
                                                          keep_cols=keep_cols)
         elif register_name == 'MaStR':
