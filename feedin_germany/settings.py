c3eae8c52587f9675ff6d58c57f638183b12e5ec
birgits <birgit.schachler@rl-institut.de>
2019-08-15 18:32:39 +0200

    Add settings.py for global paths

---
+++ feedin_germany/settings.py
@@ -0,0 +1,7 @@
+
+def init():
+    global data_path, path_era5_netcdf
+    # path to csv files
+    data_path = '/home/birgit/rli-server/04_Projekte/163_Open_FRED/03-Projektinhalte/AP7 Community/paper_data/weather_data'
+    # path to ERA5 original netcdf files
+    path_era5_netcdf = '/home/birgit/rli-daten/Wetterdaten/ERA5/'
