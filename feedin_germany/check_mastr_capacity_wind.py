ca5698f78436ae858fe2e93085b54b5f33842539
SabineH <sabine.haas@rl-institut.de>
2019-11-13 09:12:26 +0100

    minor changes

--- feedin_germany/check_mastr_capacity_wind.py
+++ feedin_germany/check_mastr_capacity_wind.py
@@ -43,13 +43,13 @@ print()
 dates = mastr_pp[['Inbetriebnahmedatum', 'DatumEndgueltigeStilllegung',
                   'GeplantesInbetriebnahmedatum',
                   'DatumBeginnVoruebergehendeStilllegung', 'DatumWiederaufnahmeBetrieb']]
-print('DatumEndgueltigeStilllegung')
 print(dates.groupby('DatumEndgueltigeStilllegung').size())
 
 
 
 
-
+print()
+print()
 print('######## MaStR typev2 year {} ########'.format(year))
 mastr_pp_year = mastr.get_mastr_pp_filtered_by_year(energy_source='Wind',
                                                     year=year)
@@ -57,8 +57,8 @@ mastr_pp_year = mastr.get_mastr_pp_filtered_by_year(energy_source='Wind',
 cap_land = mastr_pp_year.loc[mastr_pp_year['Lage'] == 'WindAnLand']['capacity'].sum() / 1e6
 cap_see = mastr_pp_year.loc[mastr_pp_year['Lage'] == 'WindAufSee']['capacity'].sum() / 1e6
 
-print('capacity Land 2017: {} GW, capacity See 2017: {}GW'.format(
-    round(cap_land, 2), round(cap_see, 2)))
+print('capacity Land {}: {} GW, capacity See {}: {}GW'.format(year,
+    round(cap_land, 2), round(cap_see, 2), year))
 
 # Lebensdauer == 20 Jahre
 mastr_pp_year_20 = mastr.get_mastr_pp_filtered_by_year(energy_source='Wind',
@@ -67,9 +67,13 @@ mastr_pp_year_20 = mastr.get_mastr_pp_filtered_by_year(energy_source='Wind',
 cap_land_20 = mastr_pp_year_20.loc[mastr_pp_year_20['Lage'] == 'WindAnLand']['capacity'].sum() / 1e6
 cap_see_20 = mastr_pp_year_20.loc[mastr_pp_year_20['Lage'] == 'WindAufSee']['capacity'].sum() / 1e6
 
-print('capacity Land 2017, decom 20: {} GW, capacity See 2017, decom 20: {} GW'.format(
-    round(cap_land_20, 2), round(cap_see_20, 2)))
+print('capacity Land {}, decom 20: {} GW, capacity See {}, decom 20: {} GW'.format(
+    year, round(cap_land_20, 2), round(cap_see_20, 2), year))
 
 # installed cap 50 hz
 cap_50hz = f.get_50hz_capacity(year=year, category='Wind') / 1e6
-print('Installed capacity 50 Hertz {} GW'.format(cap_50hz))
+print('Installed capacity 50 Hertz in {} {} GW'.format(year, cap_50hz))
+
+# diff %:
+diff = (cap_land - cap_50hz) / cap_50hz * 100
+print(diff)
