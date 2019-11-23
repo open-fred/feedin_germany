befff43a94d8f547c357921b400424b875895b8b
birgits <birgit.schachler@rl-institut.de>
2019-11-23 13:01:33 +0100

    Add smoothing parameter

--- feedin_germany/main_abschlussbericht_fred.py
+++ feedin_germany/main_abschlussbericht_fred.py
@@ -66,7 +66,7 @@ for weather_data_name in weather_data_names:
                 register_name=register_name, weather_data_name=weather_data_name,
                 debug_mode=debug_mode, wake_losses_model=wake_losses_model,
                 scale_to=scale_to, return_feedin=True, decom_20=decom_20,
-                wind_technology=wind_technology)
+                wind_technology=wind_technology, smoothing=smoothing)
             feedin.set_index('time').to_csv(os.path.join(
                 feedin_folder, 'feedin_50Hz_{}_{}_{}.csv'.format(
                     weather_data_name, register_name, year)))
